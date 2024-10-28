00b14c4c65f6d82713023c0482f435b6ca337a3b
Japple <jayhyonhan@gmail.com>
2024-10-28 17:29:36 +0000

    Done, ig

---
+++ VideoButWord.py
@@ -0,0 +1,87 @@
+import ffmpeg
+from os import path, remove
+import whisper_timestamped as whisper
+from pydub import AudioSegment
+from sys import argv
+import datetime
+from moviepy.editor import concatenate_videoclips, VideoFileClip
+
+if len(argv) != 3:
+    print(f"usage: {argv[0]} VIDEO WORD\nVIDEO should be in the folder Inputs.\nDon't input relative path e.g. Inputs/test.mp4, input the name for the video e.g. test")
+    quit()
+
+WORD = argv[2]
+
+FILE_NAME = argv[1] # file name
+
+sound = AudioSegment.from_file("Inputs/%s.mp4"%FILE_NAME, format="mp4") # video -> audio
+sound.export("Outputs/%s.wav"%FILE_NAME, format="wav")
+
+AUDIO_FILE = path.abspath("Outputs/%s.wav"%FILE_NAME) # Audio file path
+
+# Transcribe audio
+result = whisper.transcribe("base", AUDIO_FILE)
+
+# output json file
+OUT_FILE = path.abspath("Outputs/%s.json"%FILE_NAME)
+with open(OUT_FILE, "w") as f:
+    f.truncate(0)
+    f.write(str(result))
+    f.close()
+
+CustomTranscipt = ""
+
+for segment in result['segments']:
+    words = segment['words']
+    for j in words:
+        start, end = j['start'], j['end']
+        CustomTranscipt += f"{start}:{end}:{j['text'].strip()}\n"
+
+print(f"Output saved to {FILE_NAME}.tsrc")
+
+
+StrToSave = "" # Cut transcript
+
+for line in CustomTranscipt.splitlines():
+    linesSep = line.split(":")
+    if linesSep[2] == WORD:
+        StrToSave += line + "\n"
+
+OUT_FILE2 = path.abspath("Outputs/%s.tsrc"%FILE_NAME) # Custom file type where data is stored in plaintext
+# .tsrc format:
+# start:end:word
+# start:end:word
+# etc..
+
+with open(OUT_FILE2, "w") as f:
+    f.truncate(0)
+    f.write(StrToSave)
+    f.close()
+
+
+# Trim video
+
+vidL = []
+Iter = 0 # temporary iterator
+for line in StrToSave.splitlines():
+    Iter += 1
+    linesSep = line.split(":")
+    start = str(datetime.timedelta(seconds=float(linesSep[0])-0.1))
+    end = str(datetime.timedelta(seconds=float(linesSep[1])+0.1))
+    outpath = path.abspath(f"Trimmed/{Iter}.mp4")
+    if path.exists(outpath):
+        remove(outpath)
+    ffmpeg.input(f"Inputs/{argv[1]}.mp4", ss=start, to=end).output(outpath, loglevel="error").run()
+    vidL.append(outpath)
+
+def concatenate(video_clip_paths, output_path):
+    # create VideoFileClip object for each video file
+    clips = [VideoFileClip(c) for c in video_clip_paths]
+    final_clip = concatenate_videoclips(clips, method="compose")
+    # write the output video file
+    final_clip.write_videofile(output_path)
+
+# Concatenate everything
+OUT_FILE3 = path.abspath(f"Outputs/{FILE_NAME}.mp4")
+if path.exists(OUT_FILE3): remove(OUT_FILE3)
+concatenate(vidL, OUT_FILE3)
